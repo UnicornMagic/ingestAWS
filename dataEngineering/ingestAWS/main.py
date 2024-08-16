c8891b9229210f0f93134fab09fd2eaf9924ca84
UnicornMagic <andrew.kfoury@hotmail.com>
2024-08-16 20:48:55 +0200

    added stock fetching function

---
+++ dataEngineering/ingestAWS/main.py
@@ -0,0 +1,3 @@
+from packages.utils import fetch_daily, fetch_crude_oil, fetch_crude_oil_brent
+import pandas as pd
+import numpy as np
--- dataEngineering/ingestAWS/packages/utils.py
+++ dataEngineering/ingestAWS/packages/utils.py
@@ -6,7 +6,32 @@ api_key = os.getenv('AV_API_KEY')
 date_format = "%Y-%m-%d"
 
 
-def fetch_daily(currency):
+def fetch_daily_stock(stock:str):
+    # Call API with desired currency
+    url = "https://www.alphavantage.co/query"
+    params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": stock,
+    "datatype":"json",
+    "outputsize":"compact",#compact/full
+    "apikey": api_key
+    }
+
+    #Get and put respinse in Dictionary 
+    response = requests.get(url, params=params, verify=False)
+    data = response.json()
+
+    #  Transform dict to dataframe with date as index
+    df = pd.DataFrame.from_dict(data['Time Series (Daily)'], orient='index')
+    df.columns = ['open', 'high', 'low', 'close', 'volume']
+
+    # Set columns to correct format 
+    df.index = pd.to_datetime(df.index, format=date_format)
+    df = df.apply(pd.to_numeric)
+    
+    return df.sort_index(ascending=True)
+
+def fetch_daily_fx(currency):
     # Call API with desired currency
     url = "https://www.alphavantage.co/query"
     params = {
@@ -14,7 +39,7 @@ def fetch_daily(currency):
     "from_symbol": currency,
     "to_symbol": "USD",
     'datatype':'json',
-    'outputsize':'full',#compact/full
+    'outputsize':'compact',#compact/full
     "apikey": api_key
     }
 
@@ -32,7 +57,7 @@ def fetch_daily(currency):
     
     return df.sort_index(ascending=True)
 
-def fetch_weekly(currency):
+def fetch_weekly_fx(currency):
     # Call API with desired currency
     url = "https://www.alphavantage.co/query"
     params = {
@@ -40,7 +65,7 @@ def fetch_weekly(currency):
     "from_symbol": currency,
     "to_symbol": "USD",
     'datatype':'json',
-    'outputsize':'full',#compact/full
+    'outputsize':'compact',#compact/full
     "apikey": api_key
     }
 
@@ -104,3 +129,6 @@ def fetch_crude_oil_brent():
     df['value'] = pd.to_numeric(df['value'])  
     
     return df.sort_index(ascending=True)
+
+def get_yesterday(df: pd.DataFrame) -> pd.series:
+    return df.iloc[0]
